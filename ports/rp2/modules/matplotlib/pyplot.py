20e30fa63cf220846292d463a5dc6c4a3b360fca
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2022-01-05 12:06:29 +0100

    Fixed axvline and axhline

--- ports/rp2/modules/matplotlib/pyplot.py
+++ ports/rp2/modules/matplotlib/pyplot.py
@@ -1,8 +1,97 @@
-# Format for plot is {dictionary of settings}[[x axis], [y axis]]
+# Format for plot is __PLOT_PREFIX{dictionary of settings}[[x axis], [y axis]]
            
-# Format for attribute is ATTRIBUTE_PREFIXattribute(parameters)
+# Format for attribute is __ATTRIBUTE_PREFIXattribute(parameters)
 
+__PLOT_PREFIX = '%matplotlibdata --'
 __ATTRIBUTE_PREFIX = '%matplotlib --' # Prefix to recognize attribute
+
+def axhline(y=0, xmin=0, xmax=1, **kwargs):
+    """
+    Add a horizontal line across the axis.
+    Parameters
+    ----------
+    y : float, default: 0
+        y position in data coordinates of the horizontal line.
+    xmin : float, default: 0
+        Should be between 0 and 1, 0 being the far left of the plot, 1 the
+        far right of the plot.
+    xmax : float, default: 1
+        Should be between 0 and 1, 0 being the far left of the plot, 1 the
+        far right of the plot.
+    Returns
+    -------
+    `~matplotlib.lines.Line2D`
+    Other Parameters
+    ----------------
+    **kwargs
+        Valid keyword arguments are `.Line2D` properties, with the
+        exception of 'transform':
+        %(Line2D:kwdoc)s
+    See Also
+    --------
+    hlines : Add horizontal lines in data coordinates.
+    axhspan : Add a horizontal span (rectangle) across the axis.
+    axline : Add a line with an arbitrary slope.
+    Examples
+    --------
+    * draw a thick red hline at 'y' = 0 that spans the xrange::
+        >>> axhline(linewidth=4, color='r')
+    * draw a default hline at 'y' = 1 that spans the xrange::
+        >>> axhline(y=1)
+    * draw a default hline at 'y' = .5 that spans the middle half of
+        the xrange::
+        >>> axhline(y=.5, xmin=0.25, xmax=0.75)
+    """
+    kwargs['y'] = y
+    kwargs['xmin'] = xmin
+    kwargs['xmax'] = xmax
+    args = ()
+
+    print(__ATTRIBUTE_PREFIX + 'axhline'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
+
+def axvline(x=0, ymin=0, ymax=1, **kwargs):
+    """
+    Add a vertical line across the Axes.
+    Parameters
+    ----------
+    x : float, default: 0
+        x position in data coordinates of the vertical line.
+    ymin : float, default: 0
+        Should be between 0 and 1, 0 being the bottom of the plot, 1 the
+        top of the plot.
+    ymax : float, default: 1
+        Should be between 0 and 1, 0 being the bottom of the plot, 1 the
+        top of the plot.
+    Returns
+    -------
+    `~matplotlib.lines.Line2D`
+    Other Parameters
+    ----------------
+    **kwargs
+        Valid keyword arguments are `.Line2D` properties, with the
+        exception of 'transform':
+        %(Line2D:kwdoc)s
+    See Also
+    --------
+    vlines : Add vertical lines in data coordinates.
+    axvspan : Add a vertical span (rectangle) across the axis.
+    axline : Add a line with an arbitrary slope.
+    Examples
+    --------
+    * draw a thick red vline at *x* = 0 that spans the yrange::
+        >>> axvline(linewidth=4, color='r')
+    * draw a default vline at *x* = 1 that spans the yrange::
+        >>> axvline(x=1)
+    * draw a default vline at *x* = .5 that spans the middle half of
+        the yrange::
+        >>> axvline(x=.5, ymin=0.25, ymax=0.75)
+    """
+    kwargs['x'] = x
+    kwargs['ymin'] = ymin
+    kwargs['ymax'] = ymax
+    args = ()
+
+    print(__ATTRIBUTE_PREFIX + 'axvline'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
 
 def show():
     raise NotImplementedError('matplotlib.pyplot.show is not implemented yet for ALPACA.')
@@ -173,6 +262,9 @@ def polar(*args, **kwargs):
 def errorbar(*args, **kwargs):
     raise NotImplementedError('matplotlib.pyplot.errorbar is not implemented yet for ALPACA.')
 
+def grid(*args, **kwargs):
+    print(__ATTRIBUTE_PREFIX + 'grid'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
+
 def hist(*args, **kwargs):
     raise NotImplementedError('matplotlib.pyplot.hist is not implemented yet for ALPACA.')
 
@@ -185,10 +277,10 @@ def hlines(y, xmin, xmax, colors=None, linestyles='solid', label='', data=None, 
 
     args = (y, xmin, xmax)
 
-    print(__ATTRIBUTE_PREFIX + 'yticks'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
+    print(__ATTRIBUTE_PREFIX + 'hlines'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
 
 def legend(*args, **kwargs):
-    return __ATTRIBUTE_PREFIX + 'yticks'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')'
+    print(__ATTRIBUTE_PREFIX + 'legend'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
 
 def loglog(*args, **kwargs):
     raise NotImplementedError('matplotlib.pyplot.loglog is not implemented yet for ALPACA. Please use xscale and yscale.')
@@ -210,7 +302,7 @@ def plot(*args, scalex=True, scaley=True, data=None, **kwargs):
     kwargs['scaley'] = scaley
     kwargs['data'] = data
 
-    print(str(kwargs) + str([xx, yy]))
+    print(__PLOT_PREFIX+str(kwargs) + str([xx, yy]))
 
 def scatter(*args, **kwargs):
     raise NotImplementedError('matplotlib.pyplot.scatter is not implemented yet for ALPACA.')
@@ -238,6 +330,7 @@ def title(label, fontdict=None, loc=None, pad=None, *, y=None, **kwargs):
     kwargs['y'] = y
 
     print(__ATTRIBUTE_PREFIX + 'title'  + '(' + str(args).replace('(','').replace(')','') + ', '+ str(kwargs) +')')
+
 
 
 def xlabel(xlabel, fontdict=None, labelpad=None, *, loc=None, **kwargs):
