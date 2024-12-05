813847d668fd51edf65d7335edab844b46ca3913
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2024-12-05 11:03:36 +0100

    Updated DAC module to allow dual-channel use

--- ports/rp2/modules/dac.py
+++ ports/rp2/modules/dac.py
@@ -3,33 +3,52 @@ from functiongenerator import DC, _add_instr_to_wcr_array, _setup_spi
 
 class DAC:
 
-    def __init__(self, channel: str = 'A'):
-        self.dac_A = not channel in ['B', 'b']
+    def __init__(self):
+        pass
 
-    def write(self, voltage):
-        waveform = DC(voltage)
+    def write(self, voltage_a: float = None, voltage_b: float = None):
 
-        wcr_array = _add_instr_to_wcr_array(waveform.get_wcr_array(),
-                                            self.dac_A,
-                                            waveform.gain_2)
+        if voltage_a is None and voltage_b is None:
+            raise ValueError('Must specify either a voltage_a or voltage_b')
+        elif voltage_a is not None and voltage_b is None:
+            # Just use DAC A
+            voltages = [voltage_a]
+            selectors = [True]
+        elif voltage_b is not None and voltage_a is None:
+            # Just use DAC B
+            voltages = [voltage_b]
+            selectors = [False]
+        else:
+            # Use both DACs
+            voltages = [voltage_a, voltage_b]
+            selectors = [True, False]
 
         spi, CS, LDAC = _setup_spi()
+        LDAC.value(True) # Set LDAC to HIGH to keep latch closed
 
-        mv = memoryview(wcr_array)
+        for voltage, sele in zip(voltages, selectors):
+            waveform = DC(voltage)
 
-        CS.value(True)
-        LDAC.value(False)
+            wcr_array = _add_instr_to_wcr_array(waveform.get_wcr_array(),
+                                                sele,
+                                                waveform.gain_2)
+            mv = memoryview(wcr_array)
 
-        buffer = bytearray([0, 0])
+            CS.value(True)
 
-        # PREPARE ---------------------------
-        CS.value(False)
-        buffer[0] = mv[0]
-        buffer[1] = mv[1]
 
-        # WRITE POINT -----------------------------
-        spi.write(buffer)
-        CS.value(True)  # Datasheet: Chip select to end before LDAC pulse
+            buffer = bytearray([0, 0])
+
+            # PREPARE ---------------------------
+            CS.value(False)
+            buffer[0] = mv[0]
+            buffer[1] = mv[1]
+
+            # WRITE POINT -----------------------------
+            spi.write(buffer)
+            CS.value(True)  # Datasheet: Chip select to end before LDAC pulse
+
+        LDAC.value(False) # Release latch to update simultaneously
 
         return 1
 
